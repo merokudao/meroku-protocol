594d9216797703e13b9f492907d5268ddf443cd7
harshit-bitpack <harshit.sharma@bitpack.me>
2023-09-19 03:26:53 +0530

    whitelist ga

--- scripts/whitelist.js
+++ scripts/whitelist.js
@@ -1,10 +1,12 @@
 const { ethers } = require("hardhat");
-const fs = require("fs");
 
 const contractAddress = require("./config.json");
-const index = require("../utils/index.json");
-const listName = require("./list.json");
-const fileName = "./utils/index.json";
+
+const dappNameList = require("./reservedDappNames.json");
+const devNameList = require("./reservedDevNames.json");
+const appStoreNameList = require("./reservedAppStoreNames.json");
+
+
 async function addDappNames(names) {
   try {
     const dappNameList = new ethers.Contract(
@@ -18,48 +20,57 @@ async function addDappNames(names) {
   }
 }
 
+async function whitelist(listName, startIndex, stopIndex) {
+  try {
+    do{
+      console.log("whitlisting initiated from: ", startIndex, "to: ", stopIndex);
+      let listNameList = listName.slice(startIndex, startIndex+100);
+      console.log("list length: ", listNameList.length);
+      console.log(await addDappNames(listNameList));
+
+      console.log("list from name: ", listNameList[0], "to: ", listNameList[listNameList.length-1], "added successfully");
+      startIndex = startIndex+100;
+      }while(startIndex < stopIndex);
+
+} catch (e) {
+  console.log("Error in whitelist trx: ", e);
+}
+}
+
 async function main() {
   console.log("adding list of names");
   const signer = await ethers.getSigner();
   console.log("signer: ", signer.address);
 
-  try {
-
-    const args = process.argv.slice(2);
-
-// Process the newly added names
-console.log("Newly Added Names:", args);
-
-
-
-      let startIndex = index.list;
-      let stopIndex = listName.names.length;
-
-      do{
-        let listNameList = listName.names.slice(startIndex, startIndex+100);
-        console.log("listNameList Length: ", listNameList.length);
-        console.log(await addDappNames(listNameList));
-
-        console.log("startIndex: ", startIndex);
-        console.log("stopIndex: ", stopIndex);
-        console.log("list from: ", listNameList[0], "to: ", listNameList[listNameList.length-1]);
-        startIndex = startIndex+100;
-        }while(startIndex < stopIndex)
-
-        index.list = stopIndex;
-        fs.writeFileSync(
-          fileName,
-          JSON.stringify(index, null, 2),
-          function writeJSON(err) {
-            if (err) return console.log(err);
-            console.log(JSON.stringify(index));
-            console.log("writing to " + fileName);
-          }
-        );
-
-  } catch (e) {
-    console.log("Error in whitelist trx: ", e);
+  let listNameList = []
+  // fetching dapp names
+  if(dappNameList.whitelisted != dappNameList.names.length){
+    console.log("fetching newly added dapp names");
+    const list = dappNameList.names.slice(dappNameList.whitelisted, dappNameList.names.length);
+    console.log("list: ", list);
+    listNameList.push(...list)
   }
+
+  // fetching dev names
+  if(devNameList.whitelisted != devNameList.names.length){
+    console.log("fetching newly added dev names");
+    const list = devNameList.names.slice(devNameList.whitelisted, devNameList.names.length);
+    console.log("list: ", list);
+    listNameList.push(...list)
+  }
+
+  // fetching appStore names
+  if(appStoreNameList.whitelisted != appStoreNameList.names.length){
+    console.log("fetching newly added appStore names");
+    const list = appStoreNameList.names.slice(appStoreNameList.whitelisted, appStoreNameList.names.length);
+    console.log("list: ", list);
+    listNameList.push(...list)
+  }
+
+  // console.log("listNameList: ", listNameList);
+
+  await whitelist(listNameList, 0, listNameList.length);
+
 }
 
 main()
